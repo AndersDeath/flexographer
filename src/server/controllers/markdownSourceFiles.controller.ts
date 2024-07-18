1043c6e9d83928bc85102eecf1d1583833899ab7
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-07-18 20:13:38 +0200

    feat(server): update file loader

--- src/server/controllers/markdownSourceFiles.controller.ts
+++ src/server/controllers/markdownSourceFiles.controller.ts
@@ -2,28 +2,23 @@ import {Request, Response} from "express";
 import {Controller} from "../models/controller.model";
 import {fetchBodyParams} from "../utils/fetchBodyParams";
 import {upload} from '../storage';
+import * as fs from 'fs-extra';
 
 export const markdownSourceFilesController: Controller = {
     route: "/api/markdown-source-files",
     method: "POST",
     middleware: upload.array('files', 50),
     controller: async (req: Request, res: Response): Promise<void> => {
-        console.log(req.files)
+        fs.rmSync('temp', {recursive: true, force: true});
+        fs.mkdirp('temp');
+        for (const file of req.files) {
+            const content = fs.readFileSync(file.path, 'utf-8');
+            fs.writeFileSync('temp/' + file.originalname, content, 'utf8')
+        }
+
         res.send({
             numberOfFiles: req.files
         });
-
-
-        // req.on("data", (chunk) => {
-        //     console.log(chunk)
-        // });
-        // const data = fetchBodyParams(req);
-        // console.log(data);
-        // req.on("end", () => {
-        //     let fileData = Buffer.concat(data);
-        //
-        //     console.log(fileData)
-        // });
 
 
     }
