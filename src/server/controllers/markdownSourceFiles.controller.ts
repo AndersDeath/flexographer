5b457b7b5dbbc99d35cf09500d31d9c97c463ed4
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-07-21 17:58:42 +0200

    feat(sever): minor fix

--- src/server/controllers/markdownSourceFiles.controller.ts
+++ src/server/controllers/markdownSourceFiles.controller.ts
@@ -11,11 +11,13 @@ export const markdownSourceFilesController: Controller = {
     middleware: upload.fields([{name: 'files', maxCount: 100}, {name: 'images', maxCount: 100}]),
     controller: async (req: Request, res: Response): Promise<void> => {
         const {categories, targets} = fetchBodyParams(req);
-        console.log(req.files)
+        console.log(Object.keys(req.files).length)
+        console.log(req.files.files.length)
+        console.log(req.files.images.length)
         // fs.rmSync('temp', {recursive: true, force: true});
         fs.mkdirp('temp');
         for (const file of req.files.files) {
-            console.log(file)
+            // console.log(file)
             const content = fs.readFileSync(file.path, 'utf-8');
             fs.mkdirp('temp/' + categories, {recursive: true});
             fs.copyFileSync(file.path, 'temp/' + categories + '/' + file.originalname)
@@ -25,6 +27,7 @@ export const markdownSourceFilesController: Controller = {
             fs.mkdirp('temp/images', {recursive: true});
             fs.copyFileSync(file.path,'temp/images/' + file.originalname)
         }
+
         //
         setTimeout(() => {
             B3.run({
@@ -34,11 +37,12 @@ export const markdownSourceFilesController: Controller = {
                 },
                 sourcePath: "./temp"
             }).then((): void => {
+                console.log('Builder3 has done')
                 // logger.log("The work of script finished");
                 // logger.timeEnd("Builder working timer");
             });
 
-        }, 1000)
+        }, 6000)
 
 
         res.send({
