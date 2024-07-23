a5f7daafed40db0b32b6f1bdff999fd73f727849
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-07-23 22:58:34 +0200

    feat(server): start swagger integration

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,8 @@ import express, { Express } from "express";
 import  bodyParser  from "body-parser";
 import { routesInit } from "./bootstrap";
 import path from "path";
+import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
 
 const app: Express = express();
 const port: number = 3000;
@@ -9,6 +11,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: "/swagger.json",
+      },
+    })
+);
 
 app.set("views", path.join(__dirname, "./views/"));
 app.set("view engine", "ejs");
