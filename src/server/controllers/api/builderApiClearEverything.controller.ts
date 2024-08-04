b0c15b036492f4e4643f9b0d46578a16ddc439d9
Vladimir Bolshakov <andersdeath@yandex.ru>
2024-08-04 19:38:04 +0200

    feat(server): update clear everything route

--- src/server/controllers/api/builderApiClearEverything.controller.ts
+++ src/server/controllers/api/builderApiClearEverything.controller.ts
@@ -2,16 +2,23 @@ import { Request, Response } from "express";
 import { Controller } from "../../models/controller.model";
 import fs from "fs-extra";
 
+const removeDirIfExists = async (path: string) => {
+  if (fs.existsSync(path)) {
+    await fs.remove(path);
+  }
+}
+
 export const builderApiClearEverythingController: Controller = {
   route: "/builder/api/clear-everything",
   method: "POST",
   controller: async (req: Request, res: Response): Promise<void> => {
     try {
-      await fs.remove("./static2");
-      await fs.remove("./markdown2");
-      await fs.remove("./temp");
-      await fs.remove("./builder3-logs.log");
-      await fs.remove("./builder3-logs.json");
+      await removeDirIfExists('./static2');
+      await removeDirIfExists('./markdown2');
+      await removeDirIfExists('./temp');
+      await removeDirIfExists('./output');
+      await removeDirIfExists('./builder3-logs.log');
+      await removeDirIfExists('./builder3-logs.json');
       res.send({ status: "success" });
     } catch (error: any) {
       res.status(500);
